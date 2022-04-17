94ee49f13d14132f3b043da947af2c8531be0489
AllaKooalla <99720388+AllaKooalla@users.noreply.github.com>
2022-04-17 20:27:26 +0300

    add all files

---
+++ users/forms.py
@@ -0,0 +1,11 @@
+﻿from django.contrib.auth.forms import AuthenticationForm
+
+from django.contrib.auth.models import User
+
+
+class UserLoginForm(AuthenticationForm):
+
+    class Meta:
+        model = User
+        fields = ('username', 'password')
+
