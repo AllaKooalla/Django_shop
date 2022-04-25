c75730e4869a489ab5f38453bfea56cc52ae2ba2
AllaKooalla <99720388+AllaKooalla@users.noreply.github.com>
2022-04-26 02:01:17 +0300

    basket

--- products/models.py
+++ products/models.py
@@ -1,6 +1,8 @@
 from django.db import models
+# from users.models import User
+from django.contrib.auth.models import User
 
-# Create your models here.
+
 
 class ProductCategory(models.Model):
     name = models.CharField(max_length=64, unique=True)
@@ -23,3 +25,13 @@ class Product(models.Model):
 
     def __str__(self):
         return f'{self.name} | {self.category.name}'
+
+
+class Basket(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    product = models.ForeignKey(Product, on_delete=models.PROTECT)
+    quantity = models.PositiveIntegerField(default=0)
+    created_timestamp = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f'Корзина для {self.user.username} | Продукт {self.product.name}'
--- users/models.py
+++ users/models.py
@@ -3,4 +3,4 @@ from django.contrib.auth.models import AbstractUser
 
 
 # class User(AbstractUser):
-#     image = models.ImageField(upload_to='users_images', blank=True) #для добавления картинки в админке, не работает с БД
+#   image = models.ImageField(upload_to='users_images', blank=True) #для добавления картинки в админке, не работает с БД
